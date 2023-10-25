becfbef2cb84470d74fb2f07dbd44c0a6dca457e
Ariunzayaaaa <zzzaya98@gmail.com>
2023-10-25 09:14:44 +0900

    update

--- route.js
+++ route.js
@@ -43,6 +43,48 @@ router.get('/item', (req, res) => {
     res.render('item/index', data)
 })
 
+// 商品詳細
+// /item/xxx のルーティング（パスパラメーター）
+router.get('/item/:id', (req, res) => {
+    const id = req.params.id
+    // TODO: case1 RDBMS を利用する
+    // TODO: case2 APIサーバを利用する
+    // itemモデルを使って IDで商品データを取得
+    var selectItem = item.find(id)
+    var data = {
+        title: "商品詳細",
+        item: selectItem,
+    }
+    // views/item/detail.ejs にデータを渡して表示
+    res.render('item/detail', data)
+
+    // res.render('item/detail', { title: "商品詳細", item: selectItem } )
+})
+
+// POSTリクエスト
+router.post('/auth', (req, res) => {
+    // POSTデータ取得
+    var loginName = req.body.login_name
+    var password = req.body.password
+    console.log(loginName, password)
+
+    var message = "ログイン失敗"
+    // .env で設定した値で、ログインチェック
+    // TODO：データベースに接続してユーザ取得
+    // TODO：パスワードはハッシュ値でチェック
+    if (loginName == process.env.LOGIN_NAME
+        && password == process.env.PASSWORD) {
+        message = "ログイン成功"
+        //TODO ログインが成功したらユーザの状態を保存
+        //TODO ログイン後のページの転送
+    } else {
+        // TODO ログイン画面に戻す
+    }
+    res.send(message)
+})
+
+// モジュール化
+module.exports = router
 
 // 商品詳細
 // /item/xxx のルーティング（パスパラメーター）
